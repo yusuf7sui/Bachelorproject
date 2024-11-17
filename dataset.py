29d44cfa9f0f6941c7f78e5d560a6f21fae583d1
yusuf7sui <yusuf.akb07@gmail.com>
2024-11-17 13:34:56 +0100

    Added base_data in dataset.py.

--- dataset.py
+++ dataset.py
@@ -1,4 +1,31 @@
 
 '''
-TODO: Add basic Data and extended data later for validation
+TODO: Add extended data for validation and scenarios later.
 '''
+
+'''
+base data with corresponding meaning for data:
+duration, resource, [predecessors]
+'''
+base_data = [[0, 0, []],
+        [2, 2, [0]],
+        [5, 2, [0]],
+        [8, 6, [0]],
+        [1, 5, [1]],
+        [10, 5, [2]],
+        [9, 3, [2, 3]],
+        [2, 4, [0]],
+        [3, 7, [4, 5]],
+        [8, 3, [5]],
+        [6, 2, [8]],
+        [6, 4, [10]],
+        [9, 4, [8, 9]],
+        [2, 4, [7]],
+        [8, 4, [6, 13]],
+        [6, 1, [14]],
+        [10, 1, [13]],
+        [5, 3, [16]],
+        [8, 2, [12, 15]],
+        [3, 3, [17]],
+        [0, 0, [11, 18, 19]]
+        ]
