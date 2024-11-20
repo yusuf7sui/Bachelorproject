358c6b09e0aa32ac0f8065defa7be9d2ade3c7e2
yusuf7sui <yusuf.akb07@gmail.com>
2024-11-20 17:56:31 +0100

    Addded selection operators. The selection() and crossover() functions call the required operators. Adjusted comments for main.py for structure of run_genetic_algorithm() function.

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -1,16 +1,39 @@
-def selection():
+def crossover(kind_of_crossover: str):
+    if kind_of_crossover == 'one_point':
+        one_point_crossover()
+    else:
+        uniform_crossover()
+
+def one_point_crossover():
     pass
 
-def one_point_selection():
+def uniform_crossover():
     pass
 
-def uniform_selection():
+def selecttion(kind_of_selection: str):
+    if kind_of_selection == 'roulette':
+        roulette_selection()
+    else:
+        tournament_selection()
+
+def roulette_selection():
     pass
+
+def tournament_selection():
+    pass
+
 
 def calculate_fitness():
+    # call serial_SGS_with_activity_lists() to calculate fitness
+    # fitness corresponds the makespan
     pass
 
-def mutate():
+def serial_SGS_with_activity_lists():
+    pass
+
+def mutate(mutate_rate: float):
+    # switch a gene from every chromosome if mutation probability is given
+
     pass
 
 def replace():
--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 
 def run_genetic_algorithm():
+    # first need an initial_population
+
+    # select parent with selection operators
+
+    # do crossover-operation
+
+    # replace old population with new population
+
+    # until a convergence or maximum of generations
+
     pass
