70cd9b6af75471f710d05a4c9eca674887092595
yusuf7sui <yusuf.akb07@gmail.com>
2024-11-26 15:40:01 +0100

    Implemented the functions: calculate-fitness and the serial sgs with activity lists. Also adjusted in main.py script the function run_genetic_algorithm.

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -91,30 +91,81 @@ def uniform_crossover(father: list, mother: list):
                     break
     return child1, child2
 
-def selection(kind_of_selection: str):
+def selection(kind_of_selection: str, makespan_with_list: list):
     if kind_of_selection == 'roulette':
-        roulette_selection()
+        roulette_selection(makespan_with_list)
     else:
-        tournament_selection()
+        tournament_selection(makespan_with_list)
 
-def roulette_selection():
+def roulette_selection(makespan_with_list: list):
     pass
 
-def tournament_selection():
+def tournament_selection(makespan_with_list: list):
     pass
 
+def serial_SGS_with_activity_lists(g_lst: list, activities: list, rk: int):
+    sj: list = [g_lst[0]] # activity number and finish_time
+    Fj = [0]
+    Fg = {sj[0]: Fj[0]} # finish time for corresponding sj's
+    ttt = 0
+    for j in g_lst[1:len(g_lst)]:
+        # duration of j
+        pj = activities[j][0]
+        # finish time
+        fh = []
+        At = [] # j E J fj - pij <= t <Fj
+        for a in activities[j][2]:
+            fh.append(Fg[a])
+        EFj = max(fh) + pj
+        EFj_as_start = EFj - pj
+        t = list([EFj_as_start])
+        for x in sorted(Fj):
+            if EFj_as_start < x:
+                t.append(x)
+        ww = True
+        k = True
+        while ww == True:
+            for tt in t:
+                n = True
+                if k == False:
+                    break
+                if tt == t[len(t) - 1]: # if last possible in t then
+                    ww = False
+                    ttt = tt + pj
+                    break
+                idx = g_lst.index(j)
+                # adjust later maybe instead of all in range between tt and tt + pj -1, to just
+                # the finish_times if possible
+                for c in range(tt, (tt + pj)):
+                    for jj in g_lst[1:idx]:
+                        if (Fg[jj] - activities[jj][0]) <= c < Fg[jj]:
+                            At.append(jj)
+                    rkl = 0
+                    for bb in At:
+                        rkl += activities[bb][1]
+                    ass = rk - rkl
+                    rkj = activities[j][1]
+                    At = []
+                    if rkj > ass:
+                        n = False
+                if n == True:
+                    ww = False
+                    k = False
+                    ttt = tt + pj
+                    break
+        Fj.append(ttt)
+        sj.append(j)
+        Fg.update({j: ttt})
+    makespan = max(Fj)
+    print(Fg, activities, makespan, "finish_times fg, activities and makespan")
+    return Fg, makespan
 
-def calculate_fitness():
-    # call serial_SGS_with_activity_lists() to calculate fitness
-    # fitness corresponds the makespan
-    pass
-
-def serial_SGS_with_activity_lists():
-    pass
+def calculate_fitness(chromosome: list, activities: list, resource_capacity: int):
+    finish_times, makespan = serial_SGS_with_activity_lists(chromosome, activities, resource_capacity)
+    return finish_times, chromosome, makespan
 
 def mutate(mutate_rate: float):
     # switch a gene from every chromosome if mutation probability is given
-
     pass
 
 def replace():
--- main.py
+++ main.py
@@ -1,17 +1,37 @@
-from genetic_algorithm import random_initial_population, selection, crossover
+from genetic_algorithm import random_initial_population, selection, crossover, calculate_fitness
 
 import dataset as dt
 
-def run_genetic_algorithm(max_generations: int, pop_size: int, activities: list = dt.base_data):
+def run_genetic_algorithm(max_generations: int, pop_size: int, recource_capacity: int, kind_of_selection: str, kind_of_crossover: str, activities: list = dt.base_data):
     parents_pop = random_initial_population(pop_size, activities)
 
+    makespan_with_list = []
+    for chromosome in parents_pop:
+        makespan_with_list.append(calculate_fitness(chromosome, activities, recource_capacity))
+
+    cnt = 0
+    makespan_before = 0
+    needed_generations = 100
     for generation in range(max_generations):
-        # select parent with selection operators
+        if max_generations // 4 == cnt:
+            needed_generations += generation + 1
+            break
 
-        # do crossover-operation
+        children_pop = []
+        for gene in range(pop_size):
+            pool = selection(kind_of_selection, makespan_with_list)
+            # by probability make crossover-operators
+            # select parent with selection operators
 
+            # do crossover-operation
+
+            # mutate child
         # replace old population with new population
-
         # until a convergence or maximum of generations
-        pass
+        if makespan_with_list[0][2] == makespan_before:
+            cnt += 1
+        else:
+            cnt = 0
+            makespan_before = makespan_with_list[0][2]
+    # return minimal_makespan and amount of needed generations
     pass
